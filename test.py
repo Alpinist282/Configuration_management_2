46dd640533c5c65037ad16f656c5d080a88f47e6
Alpinist282 <nurpiisov.nikita282@gmail.com>
2024-12-21 11:22:01 +0300

    added code

---
+++ test.py
@@ -0,0 +1,15 @@
+import unittest
+
+class TestDependencyVisualizer(unittest.TestCase):
+    def test_get_commit_dependencies(self):
+        pass
+
+    def test_generate_plantuml_code(self):
+        pass
+
+    def test_save_to_file(self):
+        # Тест на запись в файл
+        pass
+
+if __name__ == "__main__":
+    unittest.main()
